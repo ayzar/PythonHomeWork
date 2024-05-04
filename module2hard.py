f2284fe4c8bcfff6732eb97e9b9c6e97c1af65d9
ayzar <valayzar@gmail.com>
2024-05-04 16:57:38 +0300

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,15 @@
+def password_(first_number):
+  second_number = ""
+  for i in range(1, first_number // 2 + 1):
+    for j in range(i, first_number):
+      if (i != j) and (first_number % (i + j) == 0):
+        second_number += str(i) + str(j)
+  print("Your password is: ", second_number)
+
+first_number = int(input("Enter your first number: "))
+# проверка на корректность вводных данных
+while first_number < 3 or first_number > 20:
+  first_number = int(input("Введите число в диапазоне от 3 до 20: "))
+  continue
+# print(first_number)
+password_(first_number)
