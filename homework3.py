90d79309333ea547c5d53bf0d62b03db963796d3
ayzar <valayzar@gmail.com>
2024-05-18 17:19:23 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,7 @@
+my_string = input("Введите строку: ")
+print(f"Длина строки: {len(my_string)}")
+print("Строка в верхнем регистре:\n", my_string.upper())
+print("Строка в нижнем регистре:\n", my_string.lower())
+print("Строка без пробелов:\n", my_string.replace(" ", ""))
+print("Первый символ строки: ", my_string[0])
+print("Последний символ строки: ", my_string[-1])
