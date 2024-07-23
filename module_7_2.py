081da5cb791d892dd0d9a237474b5e217ca4d630
ayzar <valayzar@gmail.com>
2024-07-24 00:06:01 +0300

    Update module_7_2.py

--- module_7_2.py
+++ module_7_2.py
@@ -1,20 +1,18 @@
 def custom_write(file_name, strings):
   # Создаем пустой словарь для хранения результатов
-  strings_positions = {}
+    strings_positions = {}
 
   # Открываем файл для записи в текстовом режиме
-  with open(file_name, 'w', encoding='utf-8') as file:
-      # Перебираем список строк
-      for line_number, string in enumerate(strings, start=1):
-          # Запоминаем текущую позицию в байтах
-          byte_position = file.tell() + 1  # на 1 больше, т.к. будет добавлен символ \n
-          # Записываем строку в файл
-          file.write(string + '\n')
-          # Сохраняем информацию о позиции и строке в словарь
-          strings_positions[(line_number, byte_position)] = string
-
+    file = open(file_name, 'w', encoding='utf-8') 
+    try:
+        for line_number, string in enumerate(strings, start=1):
+            byte_position = file.tell() + 1  # на 1 больше, т.к. будет добавлен символ \n
+            file.write(string + '\n')
+            strings_positions[(line_number, byte_position)] = string
   # Возвращаем полученный словарь
-  return strings_positions
+    finally:
+        file.close()
+    return strings_positions
 
 # Пример использования функции
 info = [
