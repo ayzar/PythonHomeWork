e2b8eead2c75aee6e858a66f9388bd99dd46efe2
ayzar <valayzar@gmail.com>
2024-07-24 07:29:09 +0300

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,10 @@
+def add_everything_up(a, b):
+  try:
+      return a + b
+  except TypeError:        
+      return str(a) + str(b)
+
+print(add_everything_up(123.456, 'строка'))  # 123.456строка
+print(add_everything_up('яблоко', 4215))     # яблоко4215
+print(add_everything_up(123.456, 7))         # 130.456
+print(add_everything_up('яблоко', 'строка')) # яблокострока      
