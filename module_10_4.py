47f101b0ab8d0491041723b75fc3af0b6bcacd4e
ayzar <valayzar@gmail.com>
2024-08-12 21:56:27 +0300

    Add files via upload

---
+++ module_10_4.py
@@ -0,0 +1,74 @@
+import threading
+import random
+import time
+from queue import Queue
+
+class Table:
+    def __init__(self, number):
+        self.number = number
+        self.guest = None
+
+class Guest(threading.Thread):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
+    def run(self):
+        # Гость задерживается случайное время от 3 до 10 секунд
+        time.sleep(random.randint(3, 10))
+
+class Cafe:
+    def __init__(self, *tables):
+        self.queue = Queue()
+        self.tables = tables
+
+    def guest_arrival(self, *guests):
+        for guest in guests:
+            # Поиск первого свободного стола
+            table = next((t for t in self.tables if t.guest is None), None)
+            if table:
+                table.guest = guest
+                guest.start()
+                print(f"{guest.name} сел(-а) за стол номер {table.number}")
+            else:
+                self.queue.put(guest)
+                print(f"{guest.name} в очереди")
+
+    def discuss_guests(self):
+        while any(t.guest is not None for t in self.tables) or not self.queue.empty():
+            for table in self.tables:
+                if table.guest:
+                    # Если гость закончил обед
+                    if not table.guest.is_alive():
+                        print(f"{table.guest.name} покушал(-а) и ушёл(ушла)")
+                        print(f"Стол номер {table.number} свободен")
+                        table.guest = None
+
+                        # Перемещение гостей из очереди к свободным столам
+                        if not self.queue.empty():
+                            next_guest = self.queue.get()
+                            table.guest = next_guest
+                            next_guest.start()
+                            print(f"{next_guest.name} вышел(-ла) из очереди и сел(-а) за стол номер {table.number}")
+
+            time.sleep(1)  # Небольшая задержка, чтобы избежать перегрузки процессора
+
+def main():
+    # Создание столов
+    tables = [Table(number) for number in range(1, 6)]
+    # Имена гостей
+    guests_names = [
+        'Maria', 'Oleg', 'Vakhtang', 'Sergey', 'Darya', 'Arman',
+        'Vitoria', 'Nikita', 'Galina', 'Pavel', 'Ilya', 'Alexandra'
+    ]
+    # Создание гостей
+    guests = [Guest(name) for name in guests_names]
+    # Заполнение кафе столами
+    cafe = Cafe(*tables)
+    # Приём гостей
+    cafe.guest_arrival(*guests)
+    # Обслуживание гостей
+    cafe.discuss_guests()
+
+if __name__ == "__main__":
+    main()
