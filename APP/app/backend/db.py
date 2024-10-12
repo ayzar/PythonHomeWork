e9cee8d8247e0a9e75114b7e6d2265da4143736d
ayzar <valayzar@gmail.com>
2024-10-12 07:39:16 +0300

    Update db.py

--- APP/app/backend/db.py
+++ APP/app/backend/db.py
@@ -2,9 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///taskmanager.db'
+# Устанавливаем URL для SQLite базы данных
+SQLALCHEMY_DATABASE_URL = "sqlite:///./taskmanager.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)  # echo=True для вывода SQL-запросов в консоль
+# Создаем движок базы данных
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}, echo=True)
+
+# Создаем локальную сессию
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Базовый класс для моделей
 Base = declarative_base()
+
+
